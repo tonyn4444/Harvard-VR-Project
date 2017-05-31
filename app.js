99548b14307bbb18565ac02135cf96f30de733cc
Tony Nguyen <tntuyen@ucdavis.edu>
2017-05-31 13:24:07 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const indexRoutes = require('./routes/index');
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.use(indexRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, process.env.IP, () => {
+	console.log('Listening on port 3000!');
+});
